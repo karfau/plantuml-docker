cd8f6893bbb02b0b06b7598e58e8f3eccbfae148
Christian Bewernitz <coder@karfau.de>
2020-11-12 20:55:42 +0100

    build: GitHub workflow to push to dockerhub (#4)

--- test-docker.sh
+++ test-docker.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 PLANTUML_VERSION=$(./plantuml-version.sh)
 # print help by default (without any arguments
-docker run karfau/plantuml-docker:$PLANTUML_VERSION
+docker run karfau/plantuml:$PLANTUML_VERSION
 
 # grep should find the version
-docker run karfau/plantuml-docker:$PLANTUML_VERSION -version | grep "$PLANTUML_VERSION"
+docker run karfau/plantuml:$PLANTUML_VERSION -version | grep "$PLANTUML_VERSION"
 
 # To test the installation of graphviz
-docker run karfau/plantuml-docker:$PLANTUML_VERSION -testdot
+docker run karfau/plantuml:$PLANTUML_VERSION -testdot
